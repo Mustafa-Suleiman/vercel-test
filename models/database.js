e387c68a51f38dc79064f8ad109bc5f1e3e3d937
mustafa-suleiman <109823798+Mustafa-Suleiman@users.noreply.github.com>
2025-03-29 22:00:45 +0200

    first commit

--- models/database.js
+++ models/database.js
@@ -13,24 +13,19 @@
 
 //   .catch(err => console.error('MongoDB connection error:', err));
 
-
 //   module.exports = 'mongodb://localhost:27017/test';
-
 
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const baseUri = process.env.MONGODB_URI;
-const dbName = process.env.DB_NAME;
+const DB_URI = process.env.MONGODB_URI;
 
-if (!baseUri || !dbName) {
-  throw new Error('MONGODB_URI or DB_NAME undefined');
+if (!DB_URI) {
+  throw new Error('MONGODB_URI undefined');
 }
 
-const uri = `${baseUri}/${dbName}`;
-
 mongoose
-  .connect(uri)
+  .connect(DB_URI)
   .then(() => console.log('Successfully connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
